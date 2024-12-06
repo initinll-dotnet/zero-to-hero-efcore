88a46e00b3de747b822b647a8d4ff9fa4b8a1db6
initinll <initinli@yahoo.com>
2024-12-06 22:39:02 +0530

    added project

---
+++ Dometrain.EFCore.DatabaseFirst/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Dometrain.EFCore.DatabaseFirst.Context;
+
+Console.WriteLine("Hello, World!");
+
+var context = new AdventureWorksContext();
+
+var query = context.Customers
+    .Where(c => c.FirstName.StartsWith("C"));
+
+foreach (var customer in query)
+{
+    Console.WriteLine($"{customer.FullName}");
+}
